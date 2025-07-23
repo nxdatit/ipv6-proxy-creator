2f5cf8f1186825af323be32fcd8b5f24df6a7af2
nxdatit <58620349+nxdatit@users.noreply.github.com>
2025-07-23 21:01:05 +0700

    Add files via upload

--- scripts/3proxy.sh
+++ scripts/3proxy.sh
@@ -9,13 +9,13 @@ detect_interface() {
 
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    URL="https://github.com/z3APA3A/3proxy/archive/0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-0.9.4
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    cp bin/3proxy /usr/local/etc/3proxy/bin/
+    cp ./scripts/init.d/3proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
     cd $WORKDIR
